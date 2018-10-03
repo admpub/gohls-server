d8add2a8c5cc19f438de7d73c822e7aacd82c33a
Hank Shen <swhbox@foxmail.com>
2018-10-03 14:03:12 +0800

    add vendor

--- buildinfo.go
+++ buildinfo.go
@@ -1,7 +1,7 @@
 package main
 
 var (
-	VERSION    = "##VERSION##"
-	COMMIT     = "##COMMIT##"
-	BUILD_TIME = "##BUILD_TIME##"
+	VERSION    = "test"
+	COMMIT     = "100b1bfaab7c5a579836c7f08b457fb9b56cdfbf"
+	BUILD_TIME = "1538546521"
 )
