6a3a8ec5895a49de4c83be725e3cf6c48667ab35
Hank Shen <swhbox@foxmail.com>
2019-07-21 09:42:41 +0800

    update

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,6 +2,6 @@ package buildinfo
 
 var (
 	VERSION    = "test"
-	COMMIT     = "b7cd5297a31c4df62b6043f01ba0740b109d4f1a"
-	BUILD_TIME = "1563673230"
+	COMMIT     = "d8caa6197a04263504d06e6ed0fd7812b67bdd3f"
+	BUILD_TIME = "1563673331"
 )
