7c5955553e27b10cc27ba3bc07d73af886eb55f3
Hank Shen <swhbox@foxmail.com>
2019-10-13 15:39:08 +0800

    update

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,12 +1,17 @@
 package utils
 
 import (
+	"os"
+	"strings"
 	"testing"
 )
 
 func TestConvert(t *testing.T) {
-	return
-	err := ConvertToMP4(`/Users/hank/Downloads/GoDownloader/index.ts`, `/Users/hank/Downloads/GoDownloader/index.mp4`)
+	tsFile := `/Users/hank/Downloads/GoDownloader/index.ts`
+	if _, err := os.Stat(tsFile); err != nil {
+		return
+	}
+	err := ConvertToMP4(tsFile, strings.TrimSuffix(tsFile, `.ts`)+`.mp4`)
 	if err != nil {
 		t.Error(err)
 	}
