52f2bea3eb7864d1b31b765df1df5cb090fcc5a0
Hank Shen <swhbox@foxmail.com>
2019-10-13 14:35:57 +0800

    update

---
+++ utils/utils.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"github.com/admpub/gohls-server/internal/hls"
+)
+
+var ConvertToMP4 = hls.ConvertToMP4
+
+func HomeDir() string {
+	return hls.HomeDir
+}
+func FFProbePath() string {
+	return hls.FFProbePath
+}
+func FFMPEGPath() string {
+	return hls.FFMPEGPath
+}
+func ComSkipINI() string {
+	return hls.ComSkipINI
+}
+func ComSkipPath() string {
+	return hls.ComSkipPath
+}
+
+func SetHomeDir(homeDir string) {
+	hls.HomeDir = homeDir
+}
+
+func SetFFProbePath(ffProbePath string) {
+	hls.FFProbePath = ffProbePath
+}
+
+func SetFFMPEGPath(ffMPEGPath string) {
+	hls.FFMPEGPath = ffMPEGPath
+}
+
+func SetComSkipINI(iniFile string) {
+	hls.ComSkipINI = iniFile
+}
+
+func SetComSkipPath(fpath string) {
+	hls.ComSkipPath = fpath
+}
