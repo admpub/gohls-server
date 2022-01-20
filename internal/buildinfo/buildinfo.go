7528fedb8d9072353eba0e9656ac1cdfdd691066
Hank Shen <swh@admpub.com>
2022-01-20 20:35:44 +0800

    update

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,6 +2,6 @@ package buildinfo
 
 var (
 	VERSION    = "v0.3.7"
-	COMMIT     = "45b74c5eccee1b6a994b040b43c7c1783d611dea"
-	BUILD_TIME = "1642680151"
+	COMMIT     = "ba3896ec74bcc1350a374d446becb2bf4c52670c"
+	BUILD_TIME = "1642681544"
 )
